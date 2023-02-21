80193129cdb15359dc585d9d0b8cf1e28bd5f8f7
itsdeemab <itsdeemab@gmail.com>
2023-02-21 12:46:00 +0200

    added a C program with a function that prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
- * This is a program has a function that prints the alphabet, in lowercase.
+ * print_alphabet - prints the alphabet, in lowercase.
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
