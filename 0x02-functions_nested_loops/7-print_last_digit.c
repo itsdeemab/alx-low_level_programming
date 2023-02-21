668514e85d55d1c9deb36bc47f253c619fe6d1b9
Its Dee Mab <itsdeemab@gmail.com>
2023-02-21 18:09:38 +0200

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,8 @@ int print_last_digit(int n)
 {
 	int z = n % 10;
 
-    if (a < 0)
-        a *= (-1);
-    _putchar(a + '0');
-    return (a);
+    if (z < 0)
+        z *= (-1);
+    _putchar(z + '0');
+    return (z);
 }
