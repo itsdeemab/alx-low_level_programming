2e76fc1aced0c3b666a2c916be96af8338d0363a
itsdeemab <itsdeemab@gmail.com>
2023-02-21 15:42:15 +0200

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@ int print_last_digit(int n)
 {
 	int a;
 
-	if(n >= 0)
+	if (n >= 0)
 	{
 		a = n % 10;
 	}
@@ -17,6 +17,5 @@ int print_last_digit(int n)
 		a = -n % 10;
 	}
 	_putchar(a + '0');
-
 	return (a);
 }
