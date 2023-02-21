7782e6b080b797ccf234becb31a6eb6b69053990
itsdeemab <itsdeemab@gmail.com>
2023-02-21 20:27:06 +0200

    a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	int result = i * j;
+	if (j == 0)
+	{
+		printf("%d", result);
+	}
+	else if (result < 10)
+	{
+		printf("   %d", result);
+	}
+	else if (result < 100)
+	{
+		printf("  %d", result);
+	}
+	else
+	{
+		printf(" %d", result);
+	}
+	}
+	putchar('\n');
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -43,3 +43,8 @@ int print_last_digit(int);
  */
 void jack_bauer(void);
 
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+
