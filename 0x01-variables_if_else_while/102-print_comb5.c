d8a0c5e2cb81e27a519855a5603cd4a4992240fd
Its Dee Mab <itsdeemab@gmail.com>
2023-02-26 11:53:34 +0200

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,35 @@
 int main(void)
 {
 	int i, j;
+	int a, b, c, d;
 
 	for (i = 0; i < 100; i++)
 	{
-	for (j = i; j < 100; j++)
-	{
-	if (i != j)
-	{
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		putchar(' ');
-		putchar(j / 10 + '0');
-		putchar(j % 10 + '0');
+		a = i / 10;
+		b = i % 10;
 
-	if (i < 99 || j < 99)
+	for (j = 0; j < 100; j++)
 	{
-		putchar(',');
-		putchar(' ');
+		c = j / 10;
+		d = j % 10;
+
+	if (a < c || (a == c && b < d))
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(32);
+		putchar(c + '0');
+		putchar(d + '0');
+
+	if (!(a == 9 && b == 8))
+	{
+		putchar(44);
+		putchar(32);
 	}
 	}
 	}
 	}
-	putchar('\n');
+		putchar(10);
+
 	return (0);
 }
