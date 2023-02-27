369b3f457b1df567296368bbf4d2ebd8e12fd756
Its Dee Mab <itsdeemab@gmail.com>
2023-02-27 11:35:13 +0200

    added a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,13 @@ int _putchar(char c);
  *reset_to_98 - takes a pointer to an int as parameter and updates the value
  */
 void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
