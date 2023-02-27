9fa85e0bb5e023a20b41f521a05addfd23265da9
Its Dee Mab <itsdeemab@gmail.com>
2023-02-27 13:31:03 +0200

    updated main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,12 +8,32 @@ int _putchar(char c);
  */
 void reset_to_98(int *n);
 
+/**
+ * swap_int - swaps the values of two integers
+ */
 void swap_int(int *a, int *b);
 
+/**
+ * _strlen - returns the length of a string
+ */
 int _strlen(char *s);
 
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ */
 void _puts(char *str);
 
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ */
 void print_rev(char *s);
 
+/**
+ * rev_string - reverses a string in place
+ */
 void rev_string(char *s);
+
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
