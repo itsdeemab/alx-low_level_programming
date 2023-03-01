2ae96a907b5d10b48810d747ba8eeb416d8b8b0b
Its Dee Mab <itsdeemab@gmail.com>
2023-03-01 14:34:32 +0200

    added a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0, i = 0;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
